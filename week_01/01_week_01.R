1ed3da157b241170a734e221817f626ca50ea9d6
Michael Kuehn <mkuehn10@gmail.com>
2018-07-05 19:52:00 -0400

    basic week one plots

---
+++ week_01/01_week_01.R
@@ -0,0 +1,97 @@
+library(tidyverse)
+library(readxl)
+library(scales)
+library(magrittr)
+library(maps)
+library(usmap)
+
+tuition_data <- read_xlsx('week_01/us_avg_tuition.xlsx')
+
+tuition_data %>%
+  gather(var, value, -State) %>%
+  group_by(State) %>%
+  summarise(avg_tuition = mean(value))
+
+tuition_data %>%
+  gather(var, value, -State) %>%
+  group_by(var) %>%
+  summarise(avg_tuition = mean(round(value, -1)))
+
+tuition_data %>%
+  mutate(
+    pct_change = round((`2015-16` - `2010-11`) / (`2010-11`) * 100, 0),
+    change_cat2 = case_when(
+      pct_change < -0.1 ~ "From -2% to -0.1%",
+      between(pct_change, 0, 2) ~ "From 0% to +2%",
+      between(pct_change, 3, 7) ~ "From +3% to +7%",
+      between(pct_change, 8, 12) ~ "From +8% to 12%",
+      between(pct_change, 13, 17) ~ "From +13% to +17%",
+      between(pct_change, 18, 22) ~ "From +18% to +22%",
+      between(pct_change, 22, 26) ~ "From +22% to +26%",
+      pct_change > 26 ~ "More than +26%"
+    ),
+    change_cat2 = factor(
+      change_cat2,
+      levels = c(
+        "From -2% to -0.1%",
+        "From 0% to +2%",
+        "From +3% to +7%",
+        "From +8% to 12%",
+        "From +13% to +17%",
+        "From +18% to +22%",
+        "From +22% to +26%",
+        "More than +26%"
+      )
+    )
+  ) %>%
+  select(State, `2015-16`, change_cat2) -> tuition_cats
+
+tuition_cats %>%
+  ggplot() +
+  geom_bar(stat = 'identity', aes(
+    x = reorder(State, `2015-16`),
+    y = `2015-16`,
+    fill = change_cat2
+  )) +
+  geom_text(aes(
+    x = reorder(State, `2015-16`),
+    label = dollar(round(`2015-16`,-1)),
+    y = `2015-16`
+  ),
+  angle = 0,
+  hjust = 0) +
+  theme(axis.text.x = element_text(
+    angle = 90,
+    vjust = 0.5,
+    hjust = 1
+  )) +
+  coord_flip() +
+  scale_fill_manual(
+    values = c(
+      "#7b91ba",
+      "#bcbdbf",
+      "#e0c791",
+      "#f3c355",
+      "#f1ad00",
+      "#e06d2c",
+      "#d22425",
+      "#9f0f19"
+    )
+  ) +
+  theme_light()
+
+plot_usmap(data = tuition_cats %>% rename(state = State), values = "change_cat2") +
+  scale_fill_manual(
+    values = c(
+      "#7b91ba",
+      "#bcbdbf",
+      "#e0c791",
+      "#f3c355",
+      "#f1ad00",
+      "#e06d2c",
+      "#d22425",
+      "#9f0f19"
+    ),
+    na.translate = FALSE
+  ) +
+  theme(legend.position = "top")
